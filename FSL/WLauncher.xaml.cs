e1a757ae790beb244c59341f65bc21492568c07a
Evan Roussin <evan.roussin@gmail.com>
2021-06-23 08:52:22 +0200

    Ajout de la solution

---
+++ FSL/WLauncher.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FSL
+{
+    /// <summary>
+    /// Logique d'interaction pour WLauncher.xaml
+    /// </summary>
+    public partial class WLauncher : Window
+    {
+        public WLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void CloseWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
